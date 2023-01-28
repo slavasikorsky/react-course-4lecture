fe9f411501f77c8443f9316edf93ec6dfd097c72
Slava Sikorsky <slava.sikorsky@gmail.com>
2023-01-28 21:59:01 +0200

    Feature: Comments and comments reply functionality

--- client/src/components/CommentsList/index.js
+++ client/src/components/CommentsList/index.js
@@ -1,67 +1,145 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import Comment from "./Comment";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/auth";
+import "./CommentsList.scss";
 
-function CommentsList(props) {
-	const API_URL = "https://dummyjson.com/comments/post/";
-
+function CommentsList({ id }) {
+	const { user } = useContext(AuthContext);
 	const [comments, setComments] = useState([]);
-	// const [postID, setPostID] = useState(props.postID);
-	const [limit, setLimit] = useState(1);
-	const { postID } = props;
+	const [newComment, setNewComment] = useState("");
 
-	const loadComments = () => {
-		axios
-			.get(API_URL + postID)
+	async function getComments() {
+		try {
+			const res = await axios.get(
+				`http://localhost:5010/posts/${id}/comments`
+			);
+			setComments(res.data);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	useEffect(() => {
+		if (id !== undefined) {
+			getComments();
+		}
+	}, [id]);
+
+	const handlerSubmit = async (e) => {
+		e.preventDefault();
+		const commentBody = {
+			text: newComment,
+			userId: user?._id || false,
+		};
+		try {
+			const res = await axios.post(
+				`http://localhost:5010/posts/${id}/comments`,
+				commentBody,
+				{
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			setComments(res.data.comments);
+			setNewComment("");
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const handlerLike = async (e, commentId) => {
+		e.preventDefault();
+		await axios
+			.patch(
+				`http://localhost:5010/posts/${id}/comments/${commentId}/like`
+			)
 			.then((res) => {
 				setComments(res.data.comments);
 			})
 			.catch((error) => {
-				console.error(error);
+				console.log(error);
 			});
 	};
 
-	useEffect(() => {
-		loadComments();
-	}, []);
+	const showCommentReplyForm = (e) => {
+		e.preventDefault();
+		const formReply = e.target.parentNode.nextSibling;
+		formReply.classList.toggle("active");
+	};
 
-	const loadAllComments = () => {
-		setLimit(comments.length);
+	const handlerCommentreply = async (e, commentId, text) => {
+		try {
+			const { data } = await axios.patch(
+				`http://localhost:5010/posts/${id}/comments/${commentId}/reply`,
+				{ text }
+			);
+			setComments(data.comments);
+		} catch (err) {
+			console.log(err);
+		}
 	};
 
 	return (
-		<div>
-			{comments.length > 0 ? (
-				<div className="comments">
-					<div className="comments-count">
-						Comments: {comments.length}
-					</div>
-					{comments &&
-						comments.map((item, index) =>
-							index < limit ? (
-								<Comment
-									key={item.id}
-									name={item.user.username}
-									body={item.body}
-								/>
-							) : (
-								false
-							)
-						)}
-					{limit === comments.length ? (
-						false
-					) : (
-						<button
-							type="button"
-							className="load-comments"
-							onClick={loadAllComments}
-						>
-							Show more {comments.length - 1} comments
-						</button>
-					)}
-				</div>
+		<div className="comments">
+			<ul className="comments-list">
+				{comments &&
+					comments.map((comment) => (
+						<li key={comment._id}>
+							<span>{comment.text}</span>
+							<span className="buttons">
+								<span>| Likes: {comment.likes}</span>
+								<button
+									type="button"
+									onClick={(e) => handlerLike(e, comment._id)}
+								>
+									Like
+								</button>
+								<button
+									type="button"
+									onClick={(e) => showCommentReplyForm(e)}
+								>
+									Reply
+								</button>
+							</span>
+
+							<form
+								className="reply-from"
+								onSubmit={(e) => {
+									e.preventDefault();
+									handlerCommentreply(
+										e,
+										comment._id,
+										e.target.reply.value
+									);
+									e.target.reply.value = "";
+								}}
+							>
+								<textarea name="reply" placeholder="Reply..." />
+								<button type="submit">Submit</button>
+							</form>
+							{comment.replies.length > 0 && (
+								<div className="comment-replies">
+									<b>Replyes:</b>
+									{comment.replies &&
+										comment.replies.map((reply) => (
+											<p>{reply}</p>
+										))}
+								</div>
+							)}
+						</li>
+					))}
+			</ul>
+			{user ? (
+				<form onSubmit={(e) => handlerSubmit(e)}>
+					<textarea
+						value={newComment}
+						onChange={(e) => setNewComment(e.target.value)}
+					/>
+					<button type="submit">Add comment</button>
+				</form>
 			) : (
-				<p>Comments not found</p>
+				<h3>Please login to post your shitty comments</h3>
 			)}
 		</div>
 	);
