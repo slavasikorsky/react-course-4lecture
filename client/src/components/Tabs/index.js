a9c5d42f970564c961f9559760c3cf1fa9d06cdb
Slava Sikorsky <slava.sikorsky@gmail.com>
2023-02-12 23:04:52 +0200

    Feature[Tab]: New component for Tabs

---
+++ client/src/components/Tabs/index.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./Tabs.scss";
+
+function Tabs({ tabs }) {
+	const [activeTab, setActiveTab] = useState(tabs[0].name);
+	return (
+		<div className="tabs">
+			{tabs &&
+				tabs.map((tab) => (
+					<button
+						type="button"
+						className={activeTab === tab.name ? "active" : ""}
+						key={tab.name}
+						onClick={() => setActiveTab(tab.name)}
+					>
+						{tab.name}
+					</button>
+				))}
+			<div className="tab-content">
+				{tabs &&
+					tabs.map((tab) =>
+						activeTab === tab.name ? tab.content : null
+					)}
+			</div>
+		</div>
+	);
+}
+
+export default Tabs;
