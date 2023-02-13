d98c0540570d9a7f7b62da4134629e4bb7cbdff1
Slava Sikorsky <slava.sikorsky@gmail.com>
2023-02-13 18:20:22 +0200

    Fix[Router]: Refactor path to pages and meta content

---
+++ client/src/pages/index.js
@@ -0,0 +1,33 @@
+import About from "./About";
+import Contact from "./Contact";
+import Dashboard from "./Dashboard";
+import Messages from "./Messages";
+import Home from "./Home";
+import NoMutch from "./NoMatch";
+import Product from "./Product";
+import Posts from "./Posts";
+import Tasks from "./Tasks";
+import Login from "./Login";
+import Profile from "./Profile";
+import Registration from "./Registration";
+import Privacy from "./Privacy";
+import Settings from "./Settings";
+import Portfolio from "./Portfolio";
+
+export {
+	About,
+	Contact,
+	Dashboard,
+	Messages,
+	Home,
+	NoMutch,
+	Product,
+	Posts,
+	Tasks,
+	Login,
+	Profile,
+	Registration,
+	Privacy,
+	Settings,
+	Portfolio,
+};
--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -6,58 +6,80 @@ import PublicLayout from "../layout/PublicLayout";
 
 import { AuthProvider } from "../context/auth";
 
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-import Dashboard from "../pages/Dashboard";
-import Home from "../pages/Home";
-import NoMutch from "../pages/NoMatch";
-import Product from "../pages/Product";
-import Posts from "../pages/Posts";
-import Tasks from "../pages/Tasks";
-import Login from "../pages/Login";
-import Profile from "../pages/Profile";
-import Registration from "../pages/Registration";
-import Privacy from "../pages/Privacy";
-import Settings from "../pages/Settings";
-import Portfolio from "../pages/Portfolio";
-import CheckLayout from "../layout/CheckLayout";
+import {
+	About,
+	Contact,
+	Dashboard,
+	Messages,
+	Home,
+	NoMutch,
+	Product,
+	Posts,
+	Tasks,
+	Login,
+	Profile,
+	Registration,
+	Privacy,
+	Settings,
+	Portfolio,
+} from "../pages";
 
+import CheckLayout from "../layout/CheckLayout";
 import PrivateRouter from "./PrivateRouter";
 import PublicRouter from "./PublicRouter";
-import Messages from "../pages/Messages";
 
-const titles = {
-	"/": "Home page",
-	"/about": "About us page",
-	"/dashboard": "Dashboard page",
-	"/settings": "Settings page",
-	"/login": "Login page",
-	"/registration": "Register page",
-	"/contact": "Contact us",
-	"/privacy": "Privacy policy page",
-	"/portfolio": "Portfolio page",
-};
-
-const description = {
-	"/": "Lorem ipsum dolor sit amet, consect",
-	"/about": "Lorem ipsum dolor sit amet, consect",
-	"/dashboard": "Vestibulum rhoncus ipsum non ultrices dapibus",
-	"/settings": "Lorem ipsum dolor sit amet, consect",
-	"/login": "Lorem ipsum dolor sit amet, consect",
-	"/registration": "Lorem ipsum dolor sit amet, consect",
-	"/contact": "Contact us page description",
-	"/privacy":
-		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus ipsum non ultrices dapibus. Etiam vel sapien at ex pulvinar luctus vel nec lacus. Morbi id magna vitae tortor dignissim imperdiet tincidunt congue libero.",
-	"/portfolio": "Portfolio page description",
+const pages = {
+	"/": {
+		title: "Home page",
+		description: "Lorem ipsum dolor sit amet, consect",
+	},
+	"/about": {
+		title: "About us page",
+		description: "Lorem ipsum dolor sit amet, consect",
+	},
+	"/dashboard": {
+		title: "Dashboard page",
+		description: "Vestibulum rhoncus ipsum non ultrices dapibus",
+	},
+	"/settings": {
+		title: "Settings page",
+		description: "Lorem ipsum dolor sit amet, consect",
+	},
+	"/login": {
+		title: "Login page",
+		description: "Lorem ipsum dolor sit amet, consect",
+	},
+	"/registration": {
+		title: "Register page",
+		description: "Lorem ipsum dolor sit amet, consect",
+	},
+	"/tasks": {
+		title: "Tasks page",
+		description: "Lorem ipsum dolor sit amet, consect",
+	},
+	"/contact": {
+		title: "Contact us",
+		description: "Contact us page description",
+	},
+	"/privacy": {
+		title: "Privacy policy page",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus ipsum non ultrices dapibus. Etiam vel sapien at ex pulvinar luctus vel nec lacus. Morbi id magna vitae tortor dignissim imperdiet tincidunt congue libero.",
+	},
+	"/portfolio": {
+		title: "Portfolio page",
+		description: "Portfolio page description",
+	},
 };
 
 function Router() {
 	const location = useLocation();
 
 	useEffect(() => {
-		document.title = titles[location.pathname] ?? "My app";
+		const page = pages[location.pathname] || {};
+		document.title = page.title || "My app";
 		document.querySelector('meta[name="description"]').content =
-			description[location.pathname] ?? "Page description here";
+			page.description || "Page description here";
 	}, [location]);
 
 	return (
